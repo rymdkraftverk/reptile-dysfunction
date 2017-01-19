5ccd4f7349fb53ba6e1cf25820004784b3539f3a
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-01-19 16:13:16 +0100

    Use the new stuff in l1 lib

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import { Core, Render, Entity, Timer } from 'l1-lite';
+import { Core, Render, Entity, Timer, Key, Debug } from 'l1-lite';
 import sprites from './sprites.json';
 
 Render.createRenderer(600, 400, sprites).then(() => {
   Core.createCore();
   Core.start();
+  Debug.initDebugTools();
+  Key.add('up');
 
   const entity = Entity.create();
   entity.sprite = Render.getSprite('anpanman');
   const { sprite } = entity;
 
+  // Set position (Pixi)
   sprite.position.y = 0;
   sprite.position.x = 0;
   sprite.width = 10;
@@ -18,12 +21,24 @@ Render.createRenderer(600, 400, sprites).then(() => {
   sprite.scale.x = 4;
   sprite.scale.y = 4;
 
+  // Make clickable (Pixi)
+  sprite.interactive = true;
+  sprite.on('mousedown', () => console.log('mousedown'))
+    .on('mouseup', () => console.log('mouseup'))
+    .on('mouseupoutside', () => console.log('mouseupoutside'))
+    .on('touchstart', () => console.log('touchstart'))
+    .on('touchend', () => console.log('touchend'))
+    .on('touchendoutside', () => console.log('touchendoutside'));
+
   Render.add(sprite);
   Core.add(entity);
 
   entity.behaviours['move-y'] = {
     run: (b, e) => {
       e.sprite.position.y += 0.5;
+      if (Key.isDown('up')){
+        e.sprite.position.y -= 1;
+      }
     }
   }
 
@@ -34,16 +49,8 @@ Render.createRenderer(600, 400, sprites).then(() => {
   }
 
   entity.behaviours['delete-move-y'] = {
-    // init: (b, e) => {
-    //   console.log("init");
-    //   e.timer = Timer.create(160, () => {
-    //       delete e.behaviours['move-y'];
-    //   });
-    // },
-    timer: Timer.create(300, (e) => {
+    timer: Timer.create(300, (b, e) => {
       delete e.behaviours['move-y'];
-      // Render.remove(e.sprite);
-      // Core.remove(e);
     }),
     run: (b, e) => {
       const { timer } = b;

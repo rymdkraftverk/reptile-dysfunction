ee3b3953a8153cffc05263e89836c8cbcadc9991
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-01-20 23:26:21 +0100

    Add controller and playerhandler entities

---
+++ src/entities/controller.js
@@ -0,0 +1,12 @@
+import { Core, Render, Entity, Gamepad} from 'l1-lite';
+
+const controller = Entity.create();
+controller.behaviours['scan-for-gamepads'] = {
+  run: (b, e) => {
+    Gamepad.run();
+  }
+}
+
+Core.add(controller);
+
+export default controller;

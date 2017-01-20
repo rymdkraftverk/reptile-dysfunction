816186932bab6086fc19be7dc3fc0ad63bad0587
Axel Ulmestig <axel.ulmestig@gmail.com>
2017-01-21 00:39:22 +0100

    moved player creation to separate file

---
+++ src/entities/player.js
@@ -0,0 +1,70 @@
+import { Render, Entity, Key, Gamepad} from 'l1-lite';
+import Matter from 'matter-js'
+
+module.exports = core => {
+  const entity = Entity.create();
+  entity.sprite = Render.getSprite('lizard1');
+  const { sprite } = entity;
+
+  // Set position (Pixi)
+  entity.body = core.Bodies.circle(0, 0, 10);
+  core.World.add(core.engine.world, [entity.body]);
+  /*
+  sprite.position.y = 0;
+  sprite.position.x = 0;
+  */
+  sprite.width = 10;
+  sprite.height = 10;
+  sprite.anchor.x = 0.5;
+  sprite.anchor.y = 0.5;
+  sprite.scale.x = 4;
+  sprite.scale.y = 4;
+
+  // Make clickable (Pixi)
+  sprite.interactive = true;
+  sprite.on('mousedown', () => console.log('mousedown'))
+    .on('mouseup', () => console.log('mouseup'))
+    .on('mouseupoutside', () => console.log('mouseupoutside'))
+    .on('touchstart', () => console.log('touchstart'))
+    .on('touchend', () => console.log('touchend'))
+    .on('touchendoutside', () => console.log('touchendoutside'));
+
+  core.Render.add(sprite);
+  core.add(entity);
+
+  entity.behaviours['movement'] = {
+    run: (b, e) => {
+      Matter.Body.setVelocity(e.body, Matter.Vector.create(x, y))
+      e.sprite.position.x = e.body.position.x;
+      e.sprite.position.y = e.body.position.y;
+      var x = 0;
+      var y = 0;
+	  //TODO Make button IDs into named constants. 
+	  //0 is left-right stick, 
+	  //1 up-down stick, 
+	  //4 left-right button, 
+	  //5 up-down stick.
+      if (Key.isDown('right')) {
+        x += 1;
+      } else if (Key.isDown('left')) {
+        x -= 1;
+      } else if (Gamepad.axisDir(0, 0) != 0) {
+        x += Gamepad.axisDir(0, 0);
+	  } else {
+		x += Gamepad.axisDir(0, 4);
+	  }
+      if (Key.isDown('down')) {
+        y += 1;
+      } else if (Key.isDown('up')) {
+        y -= 1;
+      } else if (Gamepad.axisDir(0, 1) != 0) {
+        y += Gamepad.axisDir(0, 1);
+	  } else {
+		y += Gamepad.axisDir(0, 5);
+	  }
+      const direction = Matter.Vector.create(x, y)
+      //Matter.Body.applyForce(e.body, e.body, direction);
+      Matter.Body.setVelocity(e.body, direction)
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import sprites from './sprites.json';
 
 import Matter from 'matter-js'
 
+import addPlayer from './entities/player.js'
+
 var Engine = Matter.Engine;
 var engine = Engine.create();
 var World = Matter.World;
 var Bodies = Matter.Bodies;
 engine.world.gravity.y = 0;
 Engine.run(engine);
+
+Core.engine = engine;
+Core.World = World;
+Core.Bodies = Bodies;
+
+Core.Render = Render;
 
 import playerHandler from './entities/player-handler';
 import controller from './entities/controller';
@@ -22,71 +30,8 @@ Render.createRenderer(600, 400, sprites).then(() => {
   Key.add('down');
   Key.add('left');
 
-  const entity = Entity.create();
-  entity.sprite = Render.getSprite('lizard1');
-  const { sprite } = entity;
+  addPlayer(Core);
 
-  // Set position (Pixi)
-  entity.body = Bodies.circle(0, 0, 10);
-  World.add(engine.world, [entity.body]);
-  /*
-  sprite.position.y = 0;
-  sprite.position.x = 0;
-  */
-  sprite.width = 10;
-  sprite.height = 10;
-  sprite.anchor.x = 0.5;
-  sprite.anchor.y = 0.5;
-  sprite.scale.x = 4;
-  sprite.scale.y = 4;
-
-  // Make clickable (Pixi)
-  sprite.interactive = true;
-  sprite.on('mousedown', () => console.log('mousedown'))
-    .on('mouseup', () => console.log('mouseup'))
-    .on('mouseupoutside', () => console.log('mouseupoutside'))
-    .on('touchstart', () => console.log('touchstart'))
-    .on('touchend', () => console.log('touchend'))
-    .on('touchendoutside', () => console.log('touchendoutside'));
-
-  Render.add(sprite);
-  Core.add(entity);
-
-  entity.behaviours['movement'] = {
-    run: (b, e) => {
-      Matter.Body.setVelocity(e.body, Matter.Vector.create(x, y))
-      e.sprite.position.x = e.body.position.x;
-      e.sprite.position.y = e.body.position.y;
-      var x = 0;
-      var y = 0;
-	  //TODO Make button IDs into named constants. 
-	  //0 is left-right stick, 
-	  //1 up-down stick, 
-	  //4 left-right button, 
-	  //5 up-down stick.
-      if (Key.isDown('right')) {
-        x += 1;
-      } else if (Key.isDown('left')) {
-        x -= 1;
-      } else if (Gamepad.axisDir(0, 0) != 0) {
-        x += Gamepad.axisDir(0, 0);
-	  } else {
-		x += Gamepad.axisDir(0, 4);
-	  }
-      if (Key.isDown('down')) {
-        y += 1;
-      } else if (Key.isDown('up')) {
-        y -= 1;
-      } else if (Gamepad.axisDir(0, 1) != 0) {
-        y += Gamepad.axisDir(0, 1);
-	  } else {
-		y += Gamepad.axisDir(0, 5);
-	  }
-      const direction = Matter.Vector.create(x, y)
-      //Matter.Body.applyForce(e.body, e.body, direction);
-      Matter.Body.setVelocity(e.body, direction)
-    }
-  }
 });
 
 

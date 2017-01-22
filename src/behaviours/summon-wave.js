a2a9ba8d2e58e8f6bb4b53af3b1db7bca4d4822d
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-01-22 11:23:33 +0100

    Better checking for evil player when creating wave

--- src/behaviours/summon-wave.js
+++ src/behaviours/summon-wave.js
@@ -10,7 +10,7 @@ module.exports = (controllerId) => ({
   cooldown: 0,
   run: (b, e) => {
     b.cooldown--;
-    if (getEvilPlayer() === e && Gamepad.isPressed(e.controllerId, 1) || Key.isDown('space')){ //TODO Potential errors with evil player check, cannot test. Space can _always_ fire, regardless of evil player, for debug purposes. Not currently intended for a 5th player. 
+    if (e.alignment === 'evil' && (Gamepad.isPressed(e.controllerId, 1) || Key.isDown('space'))){ //TODO Potential errors with evil player check, cannot test. Space can _always_ fire, regardless of evil player, for debug purposes. Not currently intended for a 5th player. 
       const player = e; //Should be equivialent to: Core.get(controllerId);
       const direction = Vector.rotate(Vector.create(1, 0), player.body.angle); //angle is never pi/2, so vertical waves aren't possible
       const waveStart = Vector.sub(player.body.position, Vector.mult(direction, 1000));
